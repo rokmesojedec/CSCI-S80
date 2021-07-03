18b8e110f03c262c4b26abfbf410ed009a0e8602
Rok Mesojedec <rokmesojedec@MacBook-Pro-2.local>
2021-07-03 11:02:16 +0200

    knights knaves done

--- week 1/projects/knights/puzzle.py
+++ week 1/projects/knights/puzzle.py
@@ -11,33 +11,56 @@ CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
-knowledge0 = And(
-    # TODO
-)
+knowledge0 = And()
+knowledge0.add(Implication(AKnight, And(AKnight, AKnave)))
+
+# XOR Each Character can either be a Knight or Knave, but not both
+knowledge0.add(And(Or(AKnave,AKnight), Not(And(AKnave,AKnight))))
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
-knowledge1 = And(
-    # TODO
-)
+knowledge1 = And()
+knowledge1.add(Implication(AKnight, And(BKnave, AKnave)))
+knowledge1.add(Implication(AKnave, Not(And(BKnave, AKnave))))
+
+# XOR Each Character can either be a Knight or Knave, but not both
+knowledge1.add(And(Or(AKnave,AKnight), Not(And(AKnave,AKnight))))
+knowledge1.add(And(Or(BKnave,BKnight), Not(And(BKnave,BKnight))))
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
-knowledge2 = And(
-    # TODO
-)
+knowledge2 = And()
+knowledge2.add(Implication(AKnight, Or(And(AKnight, BKnight),And(AKnave, BKnave))))
+knowledge2.add(Implication(AKnight, Or(Not(And(AKnight, BKnight)),Not(And(AKnave, BKnave)))))
+
+knowledge2.add(Implication(BKnight, And(BKnight, AKnave)))
+knowledge2.add(Implication(BKnave, Not(And(BKnave, AKnave))))
+
+# XOR Each Character can either be a Knight or Knave, but not both
+knowledge2.add(And(Or(AKnave,AKnight), Not(And(AKnave,AKnight))))
+knowledge2.add(And(Or(BKnave,BKnight), Not(And(BKnave,BKnight))))
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
-knowledge3 = And(
-    # TODO
-)
+knowledge3 = And()
+knowledge3.add(Implication(AKnight, Or(AKnave, AKnight)))
+knowledge3.add(Implication(AKnave, Not(Or(AKnave, AKnight))))
 
+knowledge3.add(Implication(BKnight, Implication(AKnight, AKnave)))
+knowledge3.add(Implication(BKnight, CKnave))
+knowledge3.add(Implication(BKnave, Not(CKnave)))
+knowledge3.add(Implication(CKnight, AKnight))
+knowledge3.add(Implication(CKnave, Not(AKnight)))
+
+# XOR Each Character can either be a Knight or Knave, but not both
+knowledge3.add(And(Or(AKnave,AKnight), Not(And(AKnave,AKnight))))
+knowledge3.add(And(Or(BKnave,BKnight), Not(And(BKnave,BKnight))))
+knowledge3.add(And(Or(CKnave,CKnight), Not(And(CKnave,CKnight))))
 
 def main():
     symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
